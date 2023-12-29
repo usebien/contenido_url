7a1ad416cdc5837685bbcfc97c45bcf1574fdbc7
usebien <112871417+usebien@users.noreply.github.com>
2023-12-29 16:48:16 -0300

    Add files via upload

---
+++ Contenido_url.py
@@ -0,0 +1,11 @@
+
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="Contenido"
+)
+
+st.markdown("#### Rest at the end, not in the middle. KB.")
+st.sidebar.success("Selecciona un tema")
+st.sidebar.image('ruben-gutierrez-uTj1lul9_Pk-unsplash.jpeg')
